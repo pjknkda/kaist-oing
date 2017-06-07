af73fb767568af360f6324638f3d59bdd76dccef
Jungkook Park <pjknkda@gmail.com>
2017-06-07 17:36:55 +0900

    integrate with bg remover

--- main3.py
+++ main3.py
@@ -25,13 +25,20 @@ def bound_to_rect(face, ratio_x=1.0, ratio_y=1.0):
 
 
 face_map_list = []
+img_fg_list = []
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 
 def img_overlay(img, faces):
-    GAUSSIAN_SIZE = (111, 111)
+    FACE_PADDING_RATIO = 0.15
+
+    MAP_FACE_SIZE = (60, 60)
+    MAP_GAUSSIAN_SIZE = (55, 55)
     MAP_CUMMULATE_LENGTH = 50
     MAP_CUMMULATE_LIMIT = 5
+
+    FG_GAUSSIAN_SIZE = (55, 55)
+    FG_CUMMULATE_LENGTH = 50
 
     img_h, img_w, _ = img.shape
 
@@ -40,12 +47,23 @@ def img_overlay(img, faces):
 
     img_orig = img
     img = cv2.resize(img, IMAGE_SIZE[:2], interpolation=cv2.INTER_AREA)
-    # img_fg = fgbg.apply(img)
+
+    img_fg = fgbg.apply(img)
+    img_fg = cv2.GaussianBlur(img_fg, FG_GAUSSIAN_SIZE, 0)
+    img_fg[np.where(img_fg > 0)] = 1
+    img_fg_list.append(img_fg)
 
     face_map = np.zeros((IMAGE_SIZE[1], IMAGE_SIZE[0]))
     for face in faces:
-        face_map[int(face['bound_top'] * ratio_y):int(face['bound_bot'] * ratio_y),
-                 int(face['bound_left'] * ratio_x): int(face['bound_right'] * ratio_x)] = 255
+        face_h = face['bound_bot'] - face['bound_top']
+        face_w = face['bound_right'] - face['bound_left']
+
+        top = max(0, int(face['bound_top'] * ratio_y - face_h * FACE_PADDING_RATIO))
+        bot = min(IMAGE_SIZE[1], int(face['bound_bot'] * ratio_y + face_h * FACE_PADDING_RATIO))
+        left = max(0, int(face['bound_left'] * ratio_x - face_w * FACE_PADDING_RATIO))
+        right = min(IMAGE_SIZE[0], int(face['bound_right'] * ratio_x + face_w * FACE_PADDING_RATIO))
+
+        face_map[top:bot, left:right] = 255
 
         cv2.polylines(
             img,
@@ -55,7 +73,15 @@ def img_overlay(img, faces):
             1  # thickness
         )
 
-    face_map = cv2.GaussianBlur(face_map, GAUSSIAN_SIZE, -0.5)  # TODO : Gaussian may not be proper
+        # face_orig = img_orig[face['bound_top']:face['bound_bot'],
+        #                      face['bound_left']:face['bound_right']]
+        # face_orig = cv2.resize(face_orig, MAP_FACE_SIZE)
+        # img[int((top + bot) / 2 - MAP_FACE_SIZE[1] / 2):
+        #     int((top + bot) / 2 + MAP_FACE_SIZE[1] / 2),
+        #     int((left + right) / 2 - MAP_FACE_SIZE[0] / 2):
+        #     int((left + right) / 2 + MAP_FACE_SIZE[0] / 2)] = face_orig
+
+    face_map = cv2.GaussianBlur(face_map, MAP_GAUSSIAN_SIZE, -0.5)  # TODO : Gaussian may not be proper
     face_map = np.array(face_map, dtype=np.uint8)
     face_map_list.append(face_map)
 
@@ -66,13 +92,25 @@ def img_overlay(img, faces):
 
     face_map_overaly = np.dstack([face_map_overlay_averaged] * 3)
     face_map_overaly = np.array(face_map_overaly, dtype=np.float) / 255
-    face_map_overaly = np.clip(face_map_overaly, 0.1, 1)
-    face_map_overaly = 1 - face_map_overaly
+    face_map_overaly = np.clip(1 - face_map_overaly, 0.1, 1)
 
-    merged_img = np.array(img * face_map_overaly, dtype=np.uint8)
+    img_fg_overlay = np.zeros((IMAGE_SIZE[1], IMAGE_SIZE[0]))
+    for iter_img_fg in img_fg_list[-FG_CUMMULATE_LENGTH:]:
+        img_fg_overlay += iter_img_fg
+    img_fg_overlay[np.where(img_fg_overlay < np.mean(img_fg_overlay))] = 0
+    img_fg_overlay[np.where(img_fg_overlay > 0)] = 1
+    img_fg_overlay = np.dstack([img_fg_overlay] * 3)
+
+    overaly = face_map_overaly * img_fg_overlay + 0.2
+    overaly = np.clip(overaly, 0, 1)
+
+    overaly_heatmap = np.array(overaly * 255, dtype=np.uint8)
+    overaly_heatmap = cv2.applyColorMap(overaly_heatmap, cv2.COLORMAP_JET)
+
+    merged_img = np.array(img * overaly, dtype=np.uint8)
 
     cv2.imshow('Oing', merged_img)
-    # cv2.imshow('Oing FG', img_fg)
+    cv2.imshow('Oing Heatmap', overaly_heatmap)
 
 
 def convert_to_3d(img, faces):
@@ -203,6 +241,6 @@ def load_dataset(name):
     cv2.destroyAllWindows()
 
 
-load_dataset('0900')
-# load_dataset('1030')
+# load_dataset('0900')
+load_dataset('1030')
 # load_dataset('1430')
